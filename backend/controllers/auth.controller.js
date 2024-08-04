497010ea3045eecb9a0f17f0fb6d14e36fb6598f
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-08-04 22:48:49 +0545

    error

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,13 +1,16 @@
+ // import User from "../models/user.model.js";
 
-
-export const signup = async (req, res) => {
- 
+export const signup = (req, res) => {
+  // Handle signup logic here
+  res.send("Signup successful");
 };
 
-export const login = async (req, res) => {
-  res.send("login")
+export const login = (req, res) => {
+  // Handle login logic here
+  res.send("Login successful");
 };
 
-export const logout = async (req, res) => {
-
+export const logout = (req, res) => {
+  // Handle logout logic here
+  res.send("Logout successful");
 };
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+
 
 const app = express();
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello workd!");
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+
+app.get("/api/auth", authRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Authentication API');
 });
-
-app.use("api/auth", authRoutes);
 
 app.listen(PORT, () => {
 	connectToMongoDB();
