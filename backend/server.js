9e8469366e0c5f1decb2ea08fe0b5239f8e2a30b
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-08-04 21:08:52 +0545

    MongoDB Setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from './routes/auth.routes.js'
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 
@@ -8,9 +9,12 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-    res.send("Hello workd!");
-})
+  res.send("Hello workd!");
+});
 
-app.use("api/auth", authRoutes)
+app.use("api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
+app.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
+});
