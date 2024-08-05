d8c22cfc7ca0d2d4ba6aafb64988fe5b7c574588
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-08-05 22:27:31 +0545

    Get Users for Sidebar Endpoint

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 
@@ -18,6 +20,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Authentication API');
