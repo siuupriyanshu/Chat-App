1387c3163b91929e65f3e823ad4f64fc93ca62a9
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-08-05 17:44:32 +0545

    Protect Route Middleware

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -13,6 +14,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
