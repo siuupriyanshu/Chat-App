097d38e7da65e3fba9383117a688a1855bccdc53
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-08-08 11:37:11 +0545

    Logout Functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
