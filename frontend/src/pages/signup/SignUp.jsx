bf846b61bf3944ec3b753a8ae653616fa70f8f38
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-08-09 19:59:23 +0545

    login functionality

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -94,8 +94,8 @@ const SignUp = () => {
           </Link>
 
           <div>
-            <button className="btn btn-block btn-sm mt-2 border border-slate-700">
-              Sign Up
+            <button className="btn btn-block btn-sm mt-2 border border-slate-700" disabled={loading}>
+              {loading ? <span className="loading loading-spinner"></span> : "Sign up"}
             </button>
           </div>
         </form>
