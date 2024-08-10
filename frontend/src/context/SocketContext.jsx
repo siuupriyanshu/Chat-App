369ea84b85136930f8b16681b0240205a26c3ad5
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-08-11 00:59:07 +0545

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
      useEffect(() => {
         if(authUser) {
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://chat-app-118q.onrender.com", {
                 query: {
                     userId: authUser._id,
                 },
