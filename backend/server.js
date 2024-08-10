0fb40cdcb9c6bd5a00bd1284130e1bba25658d25
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-08-11 00:45:21 +0545

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -7,11 +8,11 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
-
-
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
+
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5001;
 
@@ -22,11 +23,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the Authentication API');
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
-	connectToMongoDB();
-	console.log(`Server Running on port ${PORT}`);
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
 });
